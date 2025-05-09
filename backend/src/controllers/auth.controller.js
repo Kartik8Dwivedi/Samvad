6e91cbf181e4cada4a4cb0bbcca98d8a97c8a987
Kartik Dwivedi <codewithme.kartik@gmail.com>
2025-05-10 02:13:21 +0530

    updated cookie settings

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -53,9 +53,16 @@ export async function signup(req, res) {
     res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true, // prevent XSS attacks,
-      sameSite: "strict", // prevent CSRF attacks
-      secure: process.env.NODE_ENV === "production",
+      sameSite: "None", // prevent CSRF attacks
+      secure: true,
     });
+
+    // res.cookie("jwt", token, {
+    //   maxAge: 7 * 24 * 60 * 60 * 1000,
+    //   httpOnly: true, // prevent XSS attacks,
+    //   sameSite: "strict", // prevent CSRF attacks
+    //   secure: process.env.NODE_ENV === "production",
+    // });
 
     res.status(201).json({ success: true, user: newUser });
   } catch (error) {
@@ -85,8 +92,8 @@ export async function login(req, res) {
     res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true, // prevent XSS attacks,
-      sameSite: "strict", // prevent CSRF attacks
-      secure: process.env.NODE_ENV === "production",
+      sameSite: "None", // prevent CSRF attacks
+      secure: true,
     });
 
     res.status(200).json({ success: true, user });
